5b77265c1360a32420a64ccfcd4ccfc5d0e90d2e
Christopher Chacón <chchacon02@gmail.com>
2026-04-03 17:51:45 -0600

    feat: add filters for name and plate

--- src/components/CreditView.jsx
+++ src/components/CreditView.jsx
@@ -1,11 +1,23 @@
 import { useCreditViewContext } from "../contexts/CreditViewContext";
 import { useDataContext } from "../contexts/DataContext";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import FiltersSearch from "./FiltersSearch";
+import removeImg from "../imgs/remove.png";
 
 function CreditView() {
   // Contexts
   const { visibility, setVisibility } = useCreditViewContext();
   const { data } = useDataContext();
+
+  // UseStates
+  const [filterName, setFilterName] = useState("");
+  const [filterPlate, setFilterPlate] = useState("");
+
+  const filteredData = data.filter(
+    (row) =>
+      row.name?.toLowerCase().includes(filterName.toLowerCase()) &&
+      row.license_plate?.toLowerCase().includes(filterPlate.toLowerCase()),
+  );
 
   useEffect(() => {
     console.log(data);
@@ -15,11 +27,27 @@ function CreditView() {
     <div
       className={
         visibility
-          ? "absolute h-screen w-screen bg-black/75 flex justify-center items-center"
+          ? "absolute h-screen w-screen bg-black/75 flex flex-col justify-center items-center"
           : "hidden"
       }
     >
-      <section className="h-[400px] min-w-[600px] w-4/5 bg-white">
+      <header className="bg-neutral-300 h-16 w-4/5 p-2 flex justify-center items-center relative">
+        <img
+          className="h-12 absolute left-5"
+          onClick={() => setVisibility(!visibility)}
+          src={removeImg}
+          alt="x-icon"
+        />
+        <nav className="flex justify-center items-center gap-2 text-lg">
+          <li className="list-none cursor-pointer">Actuales</li>
+          <p>|</p>
+          <li className="list-none cursor-pointer">Futuros (3 dias)</li>
+        </nav>
+        <button className="absolute right-5 bg-sky-800 text-white p-2 rounded-lg">
+          Send Messages
+        </button>
+      </header>
+      <section className="h-100 min-w-150 w-4/5 bg-white overflow-x-hidden overflow-y-auto py-1 px-5">
         <table>
           <thead>
             <tr>
@@ -33,8 +61,8 @@ function CreditView() {
             </tr>
           </thead>
           <tbody>
-            {data.map((row, index) => (
-              <tr key={index}>
+            {filteredData.map((row, _) => (
+              <tr key={_}>
                 <td>{row.name}</td>
                 <td>{row.lastName}</td>
                 <td>{row.model}</td>
@@ -47,6 +75,12 @@ function CreditView() {
           </tbody>
         </table>
       </section>
+      <FiltersSearch
+        filterName={filterName}
+        setFilterName={setFilterName}
+        filterPlate={filterPlate}
+        setFilterPlate={setFilterPlate}
+      />
     </div>
   );
 }
---
+++ src/components/FiltersSearch.jsx
@@ -0,0 +1,39 @@
+function FiltersSearch({
+  filterName,
+  setFilterName,
+  filterPlate,
+  setFilterPlate,
+}) {
+  return (
+    <footer className="bg-neutral-300 h-16 w-4/5 p-2 flex gap-5 justify-between items-center relative">
+      <div className="flex gap-5 justify-center items-center px-5">
+        Filters
+        <input
+          type="text"
+          className="bg-white py-1 px-2 rounded-xl outline-none"
+          placeholder="Name..."
+          value={filterName}
+          onChange={(e) => setFilterName(e.target.value)}
+        />
+        <input
+          type="text"
+          className="bg-white py-1 px-2 rounded-xl outline-none"
+          placeholder="Placa..."
+          value={filterPlate}
+          onChange={(e) => setFilterPlate(e.target.value)}
+        />
+      </div>
+      <button
+        className="p-2 bg-red-500 text-white rounded-lg"
+        onClick={() => {
+          setFilterName("");
+          setFilterPlate("");
+        }}
+      >
+        Eliminar Filtros
+      </button>
+    </footer>
+  );
+}
+
+export default FiltersSearch;
